1e3641ef44f54edf88bcef46ada489ff546a5ef2
JODonovanKelly <JODonovanKelly@gmail.com>
2021-02-01 17:51:41 +0000

    Having Errors with Visual Studio, will wait for instruction.
    Signed-off-by: JODonovanKelly <JODonovanKelly@gmail.com>

---
+++ PracticalGamingJonathanODonovanKelly/Assets/MovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (should_move_forward()) move forward();
+
+
+    }
+
+    private void move_forward()
+    {
+        transform.position += transform.forward;
+    }
+
+    private bool should_move_forward()
+    {
+        return Input.GetKey(KeyCode.W);
+    }
+}
